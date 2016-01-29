acb35c411a240ad4243764a00837badd2d6e2f78
Holdrick <tjcooper@ucalgary.ca>
2016-01-29 10:23:13 -0700

    Basic game working. No AI implemented yet

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,19 +1,46 @@
 import pprint
 import random
 
-tttBoard = {'topL':'', 'topM':'', 'topR':'',
-            'midL':'', 'midM':'', 'midR':'',
-            'botL':'', 'botM':'', 'botR':''};
+tttBoard = {'topL':' ', 'topM':' ', 'topR':' ',
+            'midL':' ', 'midM':' ', 'midR':' ',
+            'botL':' ', 'botM':' ', 'botR':' '};
 
 validShapes = ['X', 'x', 'O', 'o'];
 
+
+def winner():
+    if tttBoard['topL'] in validShapes:
+        if tttBoard['topL'] == tttBoard['topM'] == tttBoard['topR']:
+            return True;
+        elif tttBoard['topL'] == tttBoard['midM'] == tttBoard['botR']:
+            return True;
+        elif tttBoard['topL'] == tttBoard['midL'] == tttBoard['botL']:
+            return True;
+    elif tttBoard['topM'] in validShapes:
+        if tttBoard['topM'] == tttBoard['midM'] == tttBoard['botm']:
+            return True;
+    elif tttBoard['topR'] in validShapes:
+        if tttBoard['topR'] == tttBoard['midR'] == tttBoard['botR']:
+            return True;
+        elif tttBoard['topR'] == tttBoard['midM'] == tttBoard['botL']:
+            return True;
+    elif tttBoard['midL'] in validShapes:
+        if tttBoard['midL'] == tttBoard['midM'] == tttBoard['midR']:
+            return True;
+    elif tttBoard['botL'] in validShapes:
+        if tttBoard['botL'] == tttBoard['botM'] == tttBoard['botR']:
+            return True;    
+
+    return False;
+
+
 def printBoard():
     print('');
-    print(tttBoard['topL'] + ' | ' + tttBoard['topM'] + ' | ' + tttBoard['topR']);
+    print(tttBoard['topL'] + '|' + tttBoard['topM'] + '|' + tttBoard['topR']);
     print('------');
-    print(tttBoard['midL'] + ' | ' + tttBoard['midM'] + ' | ' + tttBoard['midR']);
+    print(tttBoard['midL'] + '|' + tttBoard['midM'] + '|' + tttBoard['midR']);
     print('------');
-    print(tttBoard['botL'] + ' | ' + tttBoard['botM'] + ' | ' + tttBoard['botR']);
+    print(tttBoard['botL'] + '|' + tttBoard['botM'] + '|' + tttBoard['botR']);
     print('');
 
 
@@ -27,33 +54,42 @@ def printMenu():
 
 
 def getPlayerInput():
-    valid = false;
+    valid = False;
     position = '';
     shape = '';
 
-    while valid == false:
-        if position in tttBoard.keys() && tttBoard[position] == '':
-            valid = true;
+    while valid == False:
+        if position in tttBoard.keys() and tttBoard[position] == ' ':
+            valid = True;
         else:
             position = input('Enter a position on the board: ');
 
-    valid = false;
+    valid = False;
 
-    while valid == false:
+    while valid == False:
         if shape in validShapes:
-            valid = true;
+            valid = True;
         else:
             shape = input('Enter a shape (X or O): ');
 
     tttBoard[position] = shape;
 
-
-def winner():
     
 
-# def gameLoop():
+def gameLoop():
+    player = 'person';
+    gameOver = False;
+
+    printMenu();
+    
+    while gameOver == False:
+        getPlayerInput();
+        if winner() == True:
+            gameOver = True;
+
+        printBoard();
+
+    print('Game Over');
     
 
-
-printMenu();
-# printBoard();
+gameLoop();
